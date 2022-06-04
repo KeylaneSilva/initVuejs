38084d4d964c2e8655fba96d9d331b02ab43a433
Keylane <keylanessilva07@gmail.com>
2022-06-04 00:32:48 -0300

    diretivas v-if v-show

--- projeto-instalacaoAv/src/components/Info.vue
+++ projeto-instalacaoAv/src/components/Info.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <p>Estou trabalhando e estudando</p>
+    <p v-if="status_trabalho">Estou trabalhando e estudando no momento</p>
+    <p v-else>Não estou trabalhando e nem estudando no momento</p>
     <p>Algumas das tecnologias</p>
     <ul>
       <li>{{tecnologia1}}</li>
@@ -9,6 +10,8 @@
       <li>{{tecnologia4}}</li>
       <li>{{tecnologia5}}</li>
     </ul>
+
+    <p v-show="teste_show">Testando o v-show (parecido com o if só que não tem o else)</p>
   </div>
 </template>
 
@@ -23,6 +26,8 @@
         tecnologia3: 'Node',
         tecnologia4: 'HTML',
         tecnologia5: 'CSS',
+        status_trabalho: true,
+        teste_show: true
       }
     }
   }
