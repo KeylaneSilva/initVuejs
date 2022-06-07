17f0d5db30673e33fbbbd8a8115ce468399307fb
Keylane <keylanessilva07@gmail.com>
2022-06-07 12:23:47 -0300

    $emit

--- projeto-instalacaoAv/src/components/Picture.vue
+++ projeto-instalacaoAv/src/components/Picture.vue
@@ -1,16 +1,29 @@
 <template>
   <div>
     <img :src="picture" :alt="descricao">
+    <MudarImagem @mudar-imagem="trocarImagem"/>
   </div>
 </template>
 
 <script>
+  import MudarImagem from './MudarImagem.vue'
   export default{
     name: 'Picture',
+
+    components:{
+      MudarImagem
+    },
+
     data(){
       return{
         picture: '/img/avatar.png',
         descricao: 'Keyane Silva'
+      }
+    },
+
+    methods:{
+      trocarImagem(e){
+        this.picture = '/img/avatar2.png'
       }
     }
   }
