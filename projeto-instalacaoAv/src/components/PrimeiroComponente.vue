baa332c2f9159788f35f50cb094d6972f9301afe
Keylane <keylanessilva07@gmail.com>
2022-06-07 01:22:59 -0300

    reutilização de componentes

--- projeto-instalacaoAv/src/components/PrimeiroComponente.vue
+++ projeto-instalacaoAv/src/components/PrimeiroComponente.vue
@@ -3,18 +3,23 @@
     <h1>Primeiro Componente com Vue</h1>
     <p>Meu nome é {{ nome }} e sou {{ profissao }}</p>
     <MultiplosEventos/>
+    <ReutilizacaoComponente/>
+    <ReutilizacaoComponente/>
+    <ReutilizacaoComponente/>
   </div>
 </template>
 
 <!-- exportar componente -->
 <script>
+  import ReutilizacaoComponente from './ReutilizacaoComponente.vue'
   import MultiplosEventos from './MultiplosEventos.vue'
 
   export default {
     name: 'PrimeiroComponente',
 
     components:{
-      MultiplosEventos
+      MultiplosEventos,
+      ReutilizacaoComponente
     },
 
     data(){
---
+++ projeto-instalacaoAv/src/components/ReutilizacaoComponente.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button  @click="verificar()">Add ao carinho</button>
+    <p v-show="adicionado">{{ verify }}</p>
+  </div>
+</template>
+
+<script>
+  export default{
+    name: 'ReutilizacaoComponente',
+
+    data(){
+      return{
+        adicionado: false,
+        verify: ''
+      }
+    },
+
+    methods:{
+      verificar(e){
+        this.adicionado = !this.adicionado
+        console.log(this.verify)
+        if(this.adicionado === true){
+          this.verify = 'Adicionado'
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  div{
+    display: flex;
+  }
+</style>
