9d2f20201bfce397414bdad3ad0f7cc534abcc3b
Keylane <keylanessilva07@gmail.com>
2022-06-08 12:07:41 -0300

    ajustando rotas

---
+++ make-burguer/src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/pedidos">Pedidos</RouterLink>
+    </nav>
+    <RouterView />
+  </div>
+</template>
+
+<script>
+  export default{
+    name: 'Navbar'
+  }
+</script>
+
+<style scoped>
+
+</style>
