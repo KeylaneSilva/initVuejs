e40fff248d407b4df096ad01800fb7e7013be553
Keylane <keylanessilva07@gmail.com>
2022-06-09 14:21:16 -0300

    começando formulario

---
+++ make-burguer/src/components/BurguerForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>uma mensagem aqui</p>
+    <div>
+      <form action="" id="burger-form">
+        <div class="input-container">
+          <label for="nome">Nome do cliente:</label>
+          <input type="text" name="nome" id="nome" v-model="nome" placeholder="Digite o seu nome">
+        </div>
+         <div class="input-container">
+          <label for="pao">Escolha o seu pão:</label>
+          <select name="pao" id="pao">
+            <option value="">Escolha o seu pão</option>
+            <option value="">Pão de Batata</option>
+            <option value="">Pão de Queijo</option>
+          </select>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default{
+    name: 'BurguerForm'
+  }
+</script>
+
+<style scoped>
+
+</style>
--- make-burguer/src/views/Home.vue
+++ make-burguer/src/views/Home.vue
@@ -2,18 +2,21 @@
 <div>
   <Banner/>
   <div class="main-container">
-    <h1>Monte o seu Burguer</h1>
+    <h1>Monte o seu burguer:</h1>
+    <BurguerForm/>
   </div>
 </div>
 </template>
 
 <script>
   import Banner from '../components/Banner.vue'
+  import BurguerForm from '../components/BurguerForm.vue'
   export default{
     name: 'Home',
 
   components:{
-    Banner
+    Banner,
+    BurguerForm
    }
   }
 
